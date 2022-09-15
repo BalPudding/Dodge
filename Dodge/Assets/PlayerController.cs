09c715585932f1ef3b7ec95f1aa3c16862508156
BalPudding <buslaut@naver.com>
2022-09-15 17:31:29 +0900

    20220915

--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody; // 이동에 사용할 리지드바디 컴포넌트
+    private Rigidbody playerRigidbody; // 이동에 사용할 리지드바디 컴포넌트
     public float speed = 8f; // 이동속력
 
     void Start()
     {
-        
+        //게임 오브젝트에서 Rigidbody 컴포넌트를 찾아서
+        //playerRigidbody(변수)에 할당
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.UpArrow) == true)
+        if (Input.GetKey(KeyCode.UpArrow) == true)
         {
             //위쪽 방향키 입력이 감지된 경우 z  방향 힘 주기
             playerRigidbody.AddForce(0f, 0f, speed);
@@ -38,5 +40,11 @@ public class PlayerController : MonoBehaviour
             //왼쪽 방향키 입력이 감지된 경우 -x  방향 힘 주기
             playerRigidbody.AddForce(-speed, 0f, 0f);
         }
+    }
+
+    public void Die()
+    {
+        //자신의 게임 오브젝트를 비활성화
+        gameObject.SetActive(false);
     }
 }
